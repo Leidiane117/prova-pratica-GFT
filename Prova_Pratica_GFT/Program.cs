ec08531d0af81b5cf1c997bef19eac051cf9abca
LEIDIANE SOUZA <leidiane.souza01@fatec.sp.gov.br>
2021-02-21 18:07:39 -0300

    Criação do Projeto

---
+++ Prova_Pratica_GFT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Prova_Pratica_GFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
